1f98345611bccab31244549a444d7ea77d9fa635
Misael Lezma Mesino <misael14padilla@gmail.com>
2024-10-15 13:02:05 -0600

    [ADD]: Se agrega archivo readme

--- streamsexample/src/main/java/com/runnable/RunnableExample.java
+++ streamsexample/src/main/java/com/runnable/RunnableExample.java
@@ -3,14 +3,14 @@ package com.runnable;
 public class RunnableExample {
     public static void main(String[] args) {
         /**
-         * Runnable 
+         * Runnable
          * No recibe ningun parametro solo ejecuta una tarea
          * 
          */
 
-         Runnable runnable = ()->{
+        Runnable runnable = () -> {
             System.out.println("ejecutando tarea");
-         };
-         runnable.run();
+        };
+        runnable.run();
     }
 }
